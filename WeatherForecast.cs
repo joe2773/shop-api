a9415997bce2709b8c0d81ee7085c7f04fa61f7a
= <jbarrymarron@gmail.com>
2022-09-11 15:58:35 +1200

    initial repo commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace c_sharp_docker;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
